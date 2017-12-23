155f88e4ea882860db5dd5542c17efb8d2aec25d
tranceitionalMynd <tyler.k.cardon@gmail.com>
2017-12-22 18:03:27 -0600

    Removed copy-pasted sample code

--- spy-reaper/spy-reaper.py
+++ spy-reaper/spy-reaper.py
@@ -1,7 +1,3 @@
-from __future__ import (absolute_import, division, print_function,)
-#                        unicode_literals)
-
-import collections
 import sys
 
 # System sanity checks
@@ -10,21 +6,14 @@ if sys.version_info.major == 2:
     sys.exit(1)
 
 try:
-    import ibapi.order_condition
+    import ibapi
 except ImportError:
     try:
         sys.path.append("/opt/ibapi-beta/pythonclient")
-        import ibapi.order_condition
+        import ibapi
     except ImportError:
         sys.stderr.write("twsapi 973.05 must be installed under a system path or '/opt/ibapi-beta/pythonclient'\n")
         sys.exit(1)
-
-# Import remaining IB API modules
-from ibapi.order import (OrderComboLeg, Order)
-from ibapi.common import *
-from ibapi.tag_value import TagValue
-from ibapi import order_condition
-from ibapi.order_condition import *
 
 def main():
     pass
