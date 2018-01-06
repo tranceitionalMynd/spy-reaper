96f794d19ca601a1bed82553aa75fc77b536d4d5
Tyler Cardon <tyler.k.cardon@gmail.com>
2018-01-06 10:39:49 -0600

    Added sample transaction history MongoDB database. Use 'mongo dev/db/sample.js' to create a transaction history database named 'accountHistory'.

---
+++ dev/db/sample.js
@@ -0,0 +1,20 @@
+conn = new Mongo();
+db = conn.getDB('sample');
+db = db.getSiblingDB('accountHistory');
+
+db.transactions.find().forEach( function(thisDoc) {
+    db.transactions.remove({ "_id" : thisDoc._id });
+}); 
+
+db.transactions.insert({'ticker' : 'SPY', 'date': '2017-01-01', 'type': 'buy', 'shares': 10, 'price': 10.1 });
+db.transactions.insert({'ticker' : 'SPY', 'date': '2017-01-02', 'type': 'sell', 'shares': 10, 'price': 11.1 });
+db.transactions.insert({'ticker' : 'SPY', 'date': '2017-01-03', 'type': 'buy', 'shares': 10, 'price': 11.2 });
+db.transactions.insert({'ticker' : 'SPY', 'date': '2017-01-04', 'type': 'sell', 'shares': 10, 'price': 10.5 });
+db.transactions.insert({'ticker' : 'SPY', 'date': '2017-01-05', 'type': 'buy', 'shares': 10, 'price': 10.7 });
+db.transactions.insert({'ticker' : 'SPY', 'date': '2017-01-06', 'type': 'sell', 'shares': 10, 'price': 15.5 });
+
+allTransactions = db.transactions.find()
+
+while (allTransactions.hasNext()) {
+  printjson(allTransactions.next());
+}
